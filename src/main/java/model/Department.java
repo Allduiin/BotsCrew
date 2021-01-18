098d6c7b7c0c17d06f8c4cdc4dbbacde2c900ca4
Allduiin <vovapro01@gmail.com>
2021-01-18 12:05:44 +0200

    added lectorlist at Department

--- src/main/java/model/Department.java
+++ src/main/java/model/Department.java
@@ -1,9 +1,11 @@
 package model;
 
+import java.util.List;
 import lombok.Data;
 
 @Data
 public class Department {
+    private List<Lector> lectors;
     private String name;
     private Lector headOfDepartment;
 }
