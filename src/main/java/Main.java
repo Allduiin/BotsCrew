2c58a1ee1938a60303a6672c64b3d4ab782906aa
Allduiin <vovapro01@gmail.com>
2021-01-20 13:32:01 +0200

    Added getByName method and HeadOfDepartmentService

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,43 @@
 import config.AppConfig;
+import java.util.Arrays;
+import model.Department;
 import model.Lector;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import services.AddDepartmentService;
 import services.AddLectorService;
+import services.HeadOfDepartmentService;
 
 public class Main {
 
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        AddLectorService addLectorService = context.getBean(AddLectorService.class);
 
-        Lector lector = new Lector();
-        lector.setDegree(Lector.Degree.PROFESSOR);
-        lector.setSalary(2000L);
-        addLectorService.add(lector);
-        System.out.println(lector);
+        Lector lector1 = new Lector();
+        lector1.setDegree(Lector.Degree.PROFESSOR);
+        lector1.setSalary(2000L);
+        lector1.setName("Bogdan Shevchenko");
+        AddLectorService addLectorService = context.getBean(AddLectorService.class);
+        addLectorService.add(lector1);
+        System.out.println(lector1);
+
+        Lector lector2 = new Lector();
+        lector2.setDegree(Lector.Degree.PROFESSOR);
+        lector2.setSalary(2000L);
+        lector2.setName("Olexandr Kovalenko");
+        addLectorService.add(lector2);
+        System.out.println(lector2);
+
+        Department department = new Department();
+        department.setHeadOfDepartment(lector1);
+        department.setName("Math_Department");
+        department.setLectors(Arrays.asList(lector1,lector2));
+        AddDepartmentService addDepartmentService = context.getBean(AddDepartmentService.class);
+        System.out.println(addDepartmentService.add(department));
+
+        HeadOfDepartmentService headOfDepartmentService =
+                context.getBean(HeadOfDepartmentService.class);
+
+        headOfDepartmentService.takeHeadOfDepartment(department.getName());
     }
 }
--- src/main/java/dao/impl/DepartmentDaoImpl.java
+++ src/main/java/dao/impl/DepartmentDaoImpl.java
@@ -1,9 +1,23 @@
 package dao.impl;
 
 import dao.DepartmentDao;
+import exceptions.DataProcessingException;
 import model.Department;
+import org.hibernate.Session;
 import org.springframework.stereotype.Repository;
+import util.HibernateUtil;
 
 @Repository
 public class DepartmentDaoImpl extends EntityManagerImpl<Department> implements DepartmentDao {
+    @Override
+    public Department getByName(String nameOfDepartment) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return (Department) session
+                    .createQuery("FROM departments WHERE name =: nameOfDepartment")
+                    .setParameter("nameOfDepartment", nameOfDepartment)
+                    .uniqueResult();
+        } catch (Exception e) {
+            throw new DataProcessingException("Error getting department by name", e);
+        }
+    }
 }
